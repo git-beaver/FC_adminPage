e61fcd64828ad2a8cec653b2e2d93b539564e917
git-beaver <gml5460@naver.com>
2021-03-09 00:27:41 +0900

    FC Spring-Boot_210308

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.example.study.controller;
 
 import com.example.study.model.SearchParam;
+import com.example.study.model.network.Header;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -41,4 +42,12 @@ public class GetController { //GET Method
         
         return searchParam;
     }
+
+    @GetMapping("/header")
+    public Header getHeader() {
+
+        // {"resultCode" : "OK", "description" : "OK"}
+        return Header.builder().resultCode("OK").description("OK").build();
+    }
+
 }
